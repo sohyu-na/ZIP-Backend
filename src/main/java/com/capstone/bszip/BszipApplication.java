8f5346f65043d681d75158cea108d8806731e0e8
sohyu-na <solim73@naver.com>
2025-01-16 18:52:05 +0900

    file upload

---
+++ src/main/java/com/capstone/bszip/BszipApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.bszip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BszipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BszipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/bszip/BszipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.bszip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BszipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
