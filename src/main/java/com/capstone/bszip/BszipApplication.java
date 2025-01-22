fa6ee0d56480079821ade2c04badd8c7ceac7686
topograp2 <topo.or.dubu@gmail.com>
2025-01-22 13:20:54 +0900

    Feat: Swagger 사용을 위한 설정 완료

--- src/main/java/com/capstone/bszip/BszipApplication.java
+++ src/main/java/com/capstone/bszip/BszipApplication.java
@@ -2,8 +2,11 @@ package com.capstone.bszip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableJpaAuditing
 public class BszipApplication {
 
 	public static void main(String[] args) {
