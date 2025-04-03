a7fffdfac970fb108da8b179c1695b8a9404539d
sohyu-na <solim73@naver.com>
2025-04-03 17:03:21 +0900

    feat(TEAM-ZIP#60): 서점 리뷰 이미지 등록

--- src/main/java/com/capstone/bszip/Bookstore/service/BookstoreReviewService.java
+++ src/main/java/com/capstone/bszip/Bookstore/service/BookstoreReviewService.java
@@ -7,10 +7,12 @@ import com.capstone.bszip.Bookstore.repository.BookstoreReviewRepository;
 import com.capstone.bszip.Bookstore.service.dto.BookstoreReviewRequest;
 import com.capstone.bszip.Bookstore.service.dto.BookstoreReviewResponse;
 import com.capstone.bszip.Member.domain.Member;
+import com.capstone.bszip.cloudinary.service.CloudinaryService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,18 +22,20 @@ import java.util.stream.Collectors;
 public class BookstoreReviewService {
     private final BookstoreRepository bookstoreRepository;
     private final BookstoreReviewRepository bookstoreReviewRepository;
+    private final CloudinaryService cloudinaryService;;
 
     @Transactional
-    public BookstoreReviewResponse createReview(Member member, BookstoreReviewRequest request){
+    public BookstoreReviewResponse createReview(Member member, BookstoreReviewRequest request, MultipartFile thumbnail){
         Bookstore bookstore = bookstoreRepository.findById(request.getBookstoreId())
                 .orElseThrow(() -> new EntityNotFoundException("해당 서점을 찾을 수 없습니다."));
 
+        String cloudinaryFolderName = "bookstores_review_image";
         BookstoreReview review = new BookstoreReview();
         review.setBookstore(bookstore);
         review.setMember(member);
         review.setRating(request.getRating());
         review.setText(request.getText());
-        review.setImageUrl(request.getImageUrl());
+        review.setImageUrl(cloudinaryService.uploadfile(thumbnail, cloudinaryFolderName));
 
         bookstoreReviewRepository.save(review);
 
